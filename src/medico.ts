68f230a5c7fbc6fabb5a1397720870b66c61b5bd
juanmacruzponce <cruzponce.juan@gmail.com>
2024-05-23 19:14:49 -0300

    Backend

---
+++ src/medico.ts
@@ -0,0 +1,9 @@
+export class Medico{
+    constructor(
+        public apellido: string,
+        public nombre: string,
+        public dni: string,
+        public matricula: string,
+        public especialidad: string
+    ){}
+}
